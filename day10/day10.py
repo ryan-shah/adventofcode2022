8b5226e09859a5ad96a4a503e742db4401421f30
Ryan Shah <ryan91396@gmail.com>
2022-12-10 00:42:49 -0600

    days 9/10

---
+++ day10/day10.py
@@ -0,0 +1,51 @@
+cycles = [20, 60, 100, 140, 180, 220]
+result = []
+register = 1
+current_cycle = 0
+commands = {
+    "addx": 3,
+    "noop": 1
+}
+queue = []
+image = ['.' for i in range(40*6)]
+
+def print_image():
+    print(len(image))
+    for i in range(len(image)):
+        print(image[i], end=' ')        
+        if((i+1) % 40 == 0):
+            print()
+
+def readInput():
+    global queue
+    with open("day10/input.txt") as f:
+        lines = f.read().split("\n")
+        lines.reverse()
+        for line in lines:
+            if line.startswith("addx"):
+                val = int(line.split(" ")[1])
+                queue += [val, 0]
+            else:
+                queue += [0]
+
+def run():
+    global current_cycle, register
+    while len(queue) > 0:
+        current_cycle += 1
+        register_range = [register - 1, register, register + 1]
+        if (current_cycle-1) % 40 in register_range:
+            image[current_cycle-1] = '#'
+        if current_cycle in cycles:
+            result.append(register * current_cycle)
+        register += queue.pop()
+        #   print(current_cycle, register)
+
+def part1():
+    readInput()
+    run()
+    print(result)
+    print(sum(result))
+
+part1()
+print_image()
+
---
+++ day9/day9.py
@@ -0,0 +1,98 @@
+# Initial Grid
+grid = [['.'] * 6 for i in range(5)]
+head = 0
+tail = 1
+rope = []
+moves = []
+tail_pos = [(0,4)]
+x_moves = ['L', 'R']
+y_moves = ['U', 'D']
+
+def print_grid():
+    for j in range(len(grid)):
+        for i in range(len(grid[j])):
+            for k in range(len(rope)):
+                if rope[k][0] == i and rope[k][1] == j:
+                    print(k, end = ' ')
+                    break
+            else:
+                print(grid[j][i], end=' ')
+        print()
+
+def readInput():
+    global moves
+    with open("day9/input.txt") as f:
+        moves = [line for line in f.read().split("\n")]
+
+def updateGrid(move):
+    global rope, head, tail
+    x_dist = rope[head][0] - rope[tail][0]
+    y_dist = rope[head][1] - rope[tail][1]
+    did_move = False
+    # check move right
+    if x_dist == 2 and y_dist == 0:
+        rope[tail][0] += 1
+        did_move = True
+    # check move left
+    elif x_dist == -2 and y_dist == 0:
+        rope[tail][0] -= 1
+        did_move = True
+    # check move up
+    elif y_dist == -2 and x_dist == 0:
+        rope[tail][1] -= 1
+        did_move = True
+    # check move down
+    elif y_dist == 2 and x_dist == 0:
+        rope[tail][1] += 1
+        did_move = True
+
+    if abs(x_dist) + abs(y_dist) > 2:
+        rope[tail][0] += int(x_dist / abs(x_dist))
+        rope[tail][1] += int(y_dist / abs(y_dist))
+    
+
+
+def do_move(move):
+    global rope, head, tail
+    d, n = move.split(' ')
+    for i in range(int(n)):
+        head = 0
+        tail = 1
+        if d == 'R':
+            rope[head][0] += 1
+        elif d == 'L':
+            rope[head][0] -= 1
+        elif d == 'U':
+            rope[head][1] -= 1
+        else:
+            rope[head][1] += 1
+        for j in range(len(rope)-1):
+            if j == 1:
+                x =1
+                pass
+            head = j
+            tail = j + 1
+            updateGrid(d)
+        #print_grid()
+        #input()
+        tail_pos.append((rope[len(rope)-1][0], rope[len(rope)-1][1]))
+
+def part1():
+    global rope
+    rope = [[0, 4] for i in range(2)]
+    readInput()
+    for move in moves:
+        do_move(move)
+    print(len(set(tail_pos)))
+
+def part2():
+    global rope
+    rope = [[0, 4] for i in range(10)]
+    readInput()
+    for move in moves:
+        do_move(move)
+    print(len(set(tail_pos)))
+
+part2()
+
+
